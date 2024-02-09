99f4143934797e7608816b0cec5e854f8ee291d4
Jan <LeopoldJan@t-online.de>
2024-02-09 21:32:15 +0100

    removed some stuff

--- lua/plugins/vimfugitive.lua
+++ lua/plugins/vimfugitive.lua
@@ -2,6 +2,5 @@ return {
         "tpope/vim-fugitive",
         config = function()
                 vim.keymap.set("n","<leader>gf",vim.cmd.Git)
-                vim.keymap.set("n","<leader>gc",":Git commit")
         end
 }

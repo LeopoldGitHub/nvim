f91fccbeafe77db60e5ca2a149552f223365c082
Jan <LeopoldJan@t-online.de>
2024-02-09 22:52:07 +0100

    add lsp

--- lua/plugins/rebinds.lua
+++ lua/plugins/rebinds.lua
@@ -1,9 +1,9 @@
 vim.g.mapleader = " "
-vim.keymap.set("v", "K", ":m +1<CR>gv=gv")
-vim.keymap.set("v", "J", ":m -2<CR>gv=gv")
 vim.keymap.set("n", "<leader>gv", vim.cmd.Ex)
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("v", "K", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-e>", "<C-d>zz")
 vim.keymap.set("n", "q", "nzzzv")
 vim.keymap.set("n", "Q", "Nzzzv")

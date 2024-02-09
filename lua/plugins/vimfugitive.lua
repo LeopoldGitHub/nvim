bfbe61cc57fc4e4570e28e9cb937dc7a05928bf2
Jan <LeopoldJan@t-online.de>
2024-02-09 21:28:40 +0100

    testing stuff

--- lua/plugins/vimfugitive.lua
+++ lua/plugins/vimfugitive.lua
@@ -2,5 +2,6 @@ return {
         "tpope/vim-fugitive",
         config = function()
                 vim.keymap.set("n","<leader>gf",vim.cmd.Git)
+                vim.keymap.set("n","<leader>gc",":Git commit")
         end
 }

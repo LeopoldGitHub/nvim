dea71508c38d2dc11016f565b122d83fa75a03f2
Jan <LeopoldJan@t-online.de>
2024-02-09 23:05:48 +0100

    removed conflict

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,11 +7,7 @@ return {
         config = function()
                 require("mason").setup()
                 require("mason-lspconfig").setup({
-<<<<<<< HEAD
                         ensure_installed = { "lua_ls", "tsserver",
-=======
-                        ensure_installed = { "lua_ls"
->>>>>>> e4b09083b65f8c55a2892d042f0704a27fe332bb
                         }
                 })
                 require("mason-lspconfig").setup_handlers {

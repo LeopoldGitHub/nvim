4bab157724ff53212196c786dc3f5dc88e7de438
Jan Leopold <leopoldjan@t-online.de>
2024-02-15 18:33:01 +0100

    reinstated semanticHighlight and linked them to catppuccin in color.lua

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -14,7 +14,7 @@ return
                                 semantic_tokens = true,
                                 cmp = true,
                                 treesitter = true,
-                                harpoon= false,
+                                harpoon = false,
                                 telescope = {
                                         enabled = true,
                                 },
@@ -40,6 +40,43 @@ return
                 })
 
                 vim.cmd.colorscheme "catppuccin"
+                local links = {
+                        ['@lsp.type.namespace'] = '@namespace',
+                        ['@lsp.type.type'] = '@type',
+                        ['@lsp.type.class'] = '@type',
+                        ['@lsp.type.enum'] = '@type',
+                        ['@lsp.type.interface'] = '@type',
+                        ['@lsp.type.struct'] = '@structure',
+                        ['@lsp.type.parameter'] = '@parameter',
+                        ['@lsp.type.variable'] = '@variable',
+                        ['@lsp.type.property'] = '@property',
+                        ['@lsp.type.enumMember'] = '@constant',
+                        ['@lsp.type.function'] = '@function',
+                        ['@lsp.type.method'] = '@method',
+                        ['@lsp.type.macro'] = '@macro',
+                        ['@lsp.type.decorator'] = '@function',
+                        ['@lsp.typemod.namespace'] = '@namespace',
+                        ['@lsp.typemod.type'] = '@type',
+                        ['@lsp.typemod.class'] = '@type',
+                        ['@lsp.typemod.enum'] = '@type',
+                        ['@lsp.typemod.interface'] = '@type',
+                        ['@lsp.typemod.struct'] = '@structure',
+                        ['@lsp.typemod.parameter'] = '@parameter',
+                        ['@lsp.typemod.variable'] = '@variable',
+                        ['@lsp.typemod.property'] = '@property',
+                        ['@lsp.typemod.enumMember'] = '@constant',
+                        ['@lsp.typemod.function'] = '@function',
+                        ['@lsp.typemod.method'] = '@method',
+                        ['@lsp.typemod.macro'] = '@macro',
+                        ['@lsp.typemod.method.defaultLibrary.rust'] = '@function',
+                        ['@lsp.typemod.macro.defaultLibrary.rust'] = '@function.macro',
+                        ['@lsp.typemod.macro.library.rust'] = '@function.macro',
+                        ['@lsp.typemod.decorator'] = '@function',
+
+                }
+                for newgroup, oldgroup in pairs(links) do
+                        vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+                end
         end
 
 
